abd3566752bdb5524bdcb8f47c9ca99fe4a62d16
farah alshammari <farah.ntuf@outlook.sa>
2023-10-11 20:09:15 +0300

    Initial Commit

---
+++ Details map/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Details map
+//
+//  Created by Farah mohammed alshammari on 26/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Details map/Details_mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Details_mapApp.swift
+//  Details map
+//
+//  Created by Farah mohammed alshammari on 26/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Details_mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
