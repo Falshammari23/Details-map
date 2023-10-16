6137cae1dc8ac9be66f54f21a3a4d39f107eb40b
elham34 <elham.alwadie@gmail.com>
2023-10-16 10:52:24 +0300

    commit elham

--- Details map/test.swift
+++ Details map/test.swift
@@ -124,7 +124,7 @@ struct test: View {
                             .font(.custom("SF Pro", size: 17))
                             .position(x: 270, y: -160)
                             .padding()
-
+                        
                         Text("✔︎ Nail cut")
                             .font(.custom("SF Pro", size: 18))
                             .position(x:270, y: -130)
@@ -133,10 +133,60 @@ struct test: View {
                     }
                     
                    
-                    }
+                        
+                    
+                        ZStack{
+                            
+                            RoundedRectangle(cornerRadius: 26)
+                                .frame(width: 330, height: 202)
+                                .foregroundColor(Color.white)
+                                .shadow(color: Color.gray.opacity(0.5), radius: 4, x: 0, y: 4)
+                                .position(x: 199, y: -130)
+                            
+                            Text("Waeel Ahmed")
+                                .font(.custom("SF Pro", size: 28))
+                                .position(x:120, y: -200)
+                                .padding()
+                            Text("Experiens:")
+                                .font(.custom("SF Pro", size: 22))
+                                .position(x:90, y: -160)
+                                .padding()
+                            Text("+15 Years in Pet treatment:")
+                                .font(.custom("SF Pro", size: 17))
+                                .position(x:120, y: -135)
+                                .padding()
+                            
+                
+                            
+                     
+                                .padding()
+                            
+                            
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(width: 210, height: 40)
+                                .foregroundColor(Color.pink)
+                                .shadow(color: Color.gray.opacity(0.5), radius: 4, x: 0, y: 4)
+                                .position(x: 205, y: -240)
+                            
+                            Text("Doctors Card")
+                                .fontWeight(.bold )
+                                .foregroundColor(.white)
+                            
+                                .font(.custom("SF Pro", size: 28))
+                                .position(x: 190, y: -256)
+                                .padding()
+                            
+                            
+                            
+                            
+                        }
+                        
+                    
+                    
+                    
                     Spacer()
                     
-                   
+                }
                 }
             }
         }
